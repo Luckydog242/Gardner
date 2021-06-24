458cf725ab0121338dd51d730b90ef31631b4b11
Luckydog242 <86080990+Luckydog242@users.noreply.github.com>
2021-06-24 14:08:55 -0400

    Created a new ASP .NET Visual Studio Solution using .NET Core 3.1.
    The plan for this project is to have an "Admin Page" allowing for viewing of the database from the web (locally) such as viewing, creating, editing, and deleting the tables.
    
    Otherwise, I will create simple form that would act as a "User" page so that an author may post a blog.
    
    The Database will be created within the same solution in Visual Code.

---
+++ GardnerWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GardnerWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
